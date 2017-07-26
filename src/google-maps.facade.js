732556475ac5d9349a9729554b9e81e459c91aec
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-26 11:25:48 +0200

    add google maps facade

---
+++ src/google-maps.facade.js
@@ -0,0 +1,63 @@
+/* globals google */
+
+const geocoders = new WeakMap();
+const services = new WeakMap();
+
+class GoogleMapsFacade {
+  constructor() {
+    const geocoder = new google.maps.Geocoder();
+    geocoders.set(this, geocoder);
+
+    const placesService = new google.maps.places.PlacesService(document.createElement('div'));
+    services.set(this, placesService);
+  }
+
+  /**
+   * Gets the geocode of an address
+   * @param {string} address
+   * @returns {Promise<{lat: number, lng: number}>}
+   * @memberof GoogleMapsFacade
+   */
+  getGeocode(address) {
+    return new Promise((resolve, reject) => {
+      geocoders.get(this).geocode({
+        address,
+      }, (results, status) => {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          return reject(status);
+        }
+
+        if (results.length === 0) {
+          return reject('No place found');
+        }
+
+        return resolve(results[0].geometry.location.toJSON());
+      });
+    });
+  }
+
+  /**
+   * Returns the list of nearby bars
+   * @param {{lat: number, lng: number}} location
+   * @param {string} type
+   * @returns
+   * @memberof GoogleMapsFacade
+   */
+  getPlaces(location, type) {
+    return new Promise((resolve, reject) => {
+      services.get(this).nearbySearch({
+        location,
+        radius: '500',
+        type: [type],
+      }, (results, status) => {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          return reject(status);
+        }
+
+        return resolve(results);
+      });
+    });
+  }
+}
+
+export default new GoogleMapsFacade();
--- src/google-maps.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@google/maps';
-
-export default createClient({
-  key: 'AIzaSyBr_B7GtYnfvABFui00nhTA7sNTf2vqEZI',
-});
