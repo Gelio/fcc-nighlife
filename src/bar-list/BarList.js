4cdd8af50f52cbc373430b55c77cc9cf85682f17
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-07-26 11:27:14 +0200

    use google maps places api for bars lookup
    Uses custom Google Maps facade

--- src/bar-list/BarList.js
+++ src/bar-list/BarList.js
@@ -1,12 +1,10 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 
 import SearchBox from './search-box/SearchBox';
 import BarRow from './bar-row/BarRow';
 import Bar from './bar';
 
-import googleMaps from '../google-maps';
+import GoogleMaps from '../google-maps.facade';
 
 import './BarList.css';
 
@@ -15,81 +13,74 @@ class BarList extends Component {
     super();
 
     this.state = {
-      bars: [new Bar(1, 'Bar a', 'description'), new Bar(2, 'Bar b', 'description')],
+      bars: [],
+      isFetching: false,
+      error: null,
     };
 
     this.beginSearch = this.beginSearch.bind(this);
   }
 
-  beginSearch(address) {
-    console.log('searching near', address);
+  async beginSearch(address) {
+    this.setState({ isFetching: true, error: null, bars: [] });
 
-    console.log(googleMaps);
-    this.getGeocode(address);
-  }
+    let location;
+    try {
+      location = await GoogleMaps.getGeocode(address);
+    } catch (error) {
+      return this.setState({ isFetching: false, error });
+    }
 
-  getGeocode(address) {
-    googleMaps.geocode({
-      address,
-    }, (err, response) => {
-      const formattedAddress = response.json.results[0].formatted_address;
-      const location = response.json.results[0].geometry.location;
-      debugger;
+    let nearbyPlaces;
+    try {
+      nearbyPlaces = await GoogleMaps.getPlaces(location, 'bar');
+    } catch (error) {
+      return this.setState({ isFetching: false, error });
+    }
 
-      this.setState({
-        formattedAddress,
-      });
-      this.getPlaces(location);
-    });
-  }
-
-  getPlaces(location) {
-    googleMaps.places({
-      location,
-      type: 'bar',
-    }, (err, response) => {
-      console.log(response);
-      debugger;
+    this.setState({
+      isFetching: false,
+      bars: nearbyPlaces.map(
+        place =>
+          new Bar(place.id, `${place.name} (${place.rating})`, place.vicinity),
+      ),
     });
   }
 
   render() {
-    let content = < span > No bars found.Please enter your location above and press Search. < /span>;
-    if (this.state.bars.length > 0) {
-      content = ( <
-        ul className = "bar-list" > {
-          this.state.bars.map(bar =>
-            ( < li key = {
-                bar.id
-              } >
-              <
-              BarRow bar = {
-                bar
-              }
-              /> < /
-              li > ),
-          )
-        } <
-        /ul>
+    const { bars, error, isFetching } = this.state;
+    let content = (
+      <span> Please enter your location above and press Search. </span>
+    );
+
+    if (!isFetching && bars.length > 0) {
+      content = (
+        <ul className="bar-list">
+          {' '}{bars.map(bar =>
+            <li key={bar.id}>
+              <BarRow bar={bar} />{' '}
+            </li>,
+          )}{' '}
+        </ul>
       );
     }
 
-    return ( <
-      div >
-      <
-      SearchBox search = {
-        this.beginSearch
-      }
-      /> <
-      div className = "bar-list-wrapper container" >
-      <
-      h2 > Bars near you < /h2>
+    if (isFetching) {
+      content = <div>Loading...</div>;
+    }
 
-      {
-        content
-      } <
-      /div> < /
-      div >
+    if (error) {
+      content = <div>Request error.</div>;
+    }
+
+    return (
+      <div>
+        <SearchBox search={this.beginSearch} disabled={isFetching} />{' '}
+        <div className="bar-list-wrapper container">
+          <h2> Bars near you </h2>
+          {content}{' '}
+        </div>{' '}
+      </div>
     );
   }
 }
